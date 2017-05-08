27cd8ae9b48ef79f5d26227e4113ffc423b47ec8
Broda Noel <noel@olapic.com>
2017-05-08 01:00:36 -0300

    Implement Card in CardSelectionPage

--- src/pages/CardSelectionPage/CardSelectionPage.jsx
+++ src/pages/CardSelectionPage/CardSelectionPage.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './styles.css';
 
+import { Card } from '../../components/Card';
+
 export class CardSelectionPage extends Component {
+  // TODO: Move it to some kind of implementation
+  cards = [
+    { name: 'ReactJS', hashtags: ['#reactJS', '#react'] },
+    { name: 'AngularJS', hashtags: ['#angularjs', '#angular'] },
+    { name: 'VueJS', hashtags: ['#vuejs', '#vue'] },
+    { name: 'PHP', hashtags: ['#php'] }
+  ];
+
   render() {
     return (
       <div className="CardSelectionPage">
-        <h1>Woho!</h1>
+        {
+          this.cards.map((card) => {
+            return (
+              <Card
+                key={card.name}
+                {...card} />
+            )
+          })
+        }
       </div>
     );
   }
