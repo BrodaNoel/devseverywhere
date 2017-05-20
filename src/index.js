2a63a25810b33747d9148ebb7311075bddf5aa61
Broda Noel <brodanoel@gmail.com>
2017-05-19 23:15:57 -0300

    Add firebase hosting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom'
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <HashRouter basename="/devseverywhere/">
-    <App />
-  </HashRouter>,
+    <App />,
   document.getElementById('root')
 );
