b7c96bf70037e4b58ebbdd8aa0cf718f1e4eb42e
Broda Noel <brodanoel@gmail.com>
2017-05-19 22:43:25 -0300

    Remove the `foo` base routing

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <HashRouter basename="/pepe">
+  <HashRouter basename="/devseverywhere/">
     <App />
   </HashRouter>,
   document.getElementById('root')
